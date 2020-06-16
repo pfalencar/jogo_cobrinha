09ffd4c77e521024c598852ad8dda8c6dece0e94
pfalencar <patricia.alencar@etec.sp.gov.br>
2020-06-16 10:16:47 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBackground(){
     //cor do jogo
@@ -9,4 +14,12 @@ function criarBackground(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha(){
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBackground();
+criarCobrinha();
