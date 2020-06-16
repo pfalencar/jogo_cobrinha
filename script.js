88b50046ce9b280fd2a9163e6fafc8e9b577aeca
pfalencar <patricia.alencar@etec.sp.gov.br>
2020-06-16 12:00:07 -0300

    definir movimentos cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 
 function criarBackground(){
     //cor do jogo
@@ -21,5 +23,50 @@ function criarCobrinha(){
     }
 }
 
-criarBackground();
-criarCobrinha();
+document.addEventListener('keydown', update); 
+
+function update (event) {
+    if(event.keyCode == 37 && direction != "left") {
+        //37 é o codigo da tecla que vai pra direita
+        direction = "left";
+    }
+    if(event.keyCode == 38 && direction != "up") {
+        direction = "up";
+    }
+    if(event.keyCode == 39 && direction != "right") {
+        direction = "right";
+    }
+    if(event.keyCode == 40 && direction != "down") {
+        direction = "down";
+    }
+}
+
+function iniciarJogo() {
+    criarBackground();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box; 
+
+    snake.pop();
+     
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    //método que sempre acrescenta um elemento no começo 
+    //do vetor.
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
